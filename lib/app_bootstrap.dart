de3b3ad8008293bc1f89423afe3d7ba0ab889f2f
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-01-07 12:11:34 +0500

    setup appBootstrap classs and initilization logic

---
+++ lib/app_bootstrap.dart
@@ -0,0 +1,35 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:quiz_app/main.dart';
+
+class AppBootstrap {
+  /// Create the root widget that should be passed to [runApp].
+  Widget createRootWidget() {
+    return const MyApp();
+  }
+
+  // Register Flutter error handlers
+  void registerErrorHandlers(errorLogger) {
+    // * Show some error UI if any uncaught exception happens
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FlutterError.presentError(details);
+      errorLogger.logError(details.exception, details.stack);
+    };
+    // * Handle errors from the underlying platform/OS
+    PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
+      errorLogger.logError(error, stack);
+      return true;
+    };
+    // * Show some error UI when any widget in the app fails to build
+    ErrorWidget.builder = (FlutterErrorDetails details) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('An error occurred'),
+        ),
+        body: Center(child: Text(details.toString())),
+      );
+    };
+  }
+}
