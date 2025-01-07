a034d189b6f8a946dcfbfbcbaa63ba3ca7e09d9c
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-01-07 13:20:24 +0500

    start screen added

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/features/start_quiz/presentation/start_quiz.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,33 +8,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        // * Use this to toggle Material 3 (defaults to true since Flutter 3.16)
-        useMaterial3: true,
-        primarySwatch: Colors.grey,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.black87,
-          foregroundColor: Colors.white,
-          elevation: 0,
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
+          // * Use this to toggle Material 3 (defaults to true since Flutter 3.16)
+          useMaterial3: true,
+          primarySwatch: Colors.grey,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.deepPurpleAccent,
+            foregroundColor: Colors.white,
+            elevation: 0,
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black, // background (button) color
+              foregroundColor: Colors.white, // foreground (text) color
+            ),
+          ),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
             backgroundColor: Colors.black, // background (button) color
             foregroundColor: Colors.white, // foreground (text) color
           ),
-        ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Colors.black, // background (button) color
-          foregroundColor: Colors.white, // foreground (text) color
-        ),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Quiz App'),
-        ),
-        body: const Center(
-          child: Text('Welcome to the Quiz App!'),
-        ),
-      ),
+          scaffoldBackgroundColor: Colors.deepPurpleAccent),
+      home: const StartQuiz(),
     );
   }
 }
