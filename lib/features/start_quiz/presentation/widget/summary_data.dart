0e217485fe6901228a5b0ba3ff92ada1ff538f9d
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-01-23 14:21:43 +0500

    redesign the scroll in summarywidget

--- lib/features/start_quiz/presentation/widget/summary_data.dart
+++ lib/features/start_quiz/presentation/widget/summary_data.dart
@@ -7,21 +7,62 @@ class SummaryData extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
+      //todo:add silver scrollable
       child: SingleChildScrollView(
         child: Column(
           children: summary.map((data) {
             return Row(
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
               children: [
-                Text(((data['question_index'] as int) + 1).toString()),
+                Container(
+                  width: 30,
+                  height: 30,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: data['correct_answer'] == data['choosan_answer']
+                        ? Colors.green.shade500
+                        : Colors.red.shade100,
+                  ),
+                  child: Text(
+                    ((data['question_index'] as int) + 1).toString(),
+                    style: TextStyle(
+                      color: data['correct_answer'] == data['choosan_answer']
+                          ? Colors.white
+                          : Colors.black,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
                 Expanded(
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(data['questions'].toString()),
+                      Text(
+                        data['questions'].toString(),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
                       const SizedBox(
                         height: 5,
                       ),
-                      Text(data['correct_answer'].toString()),
-                      Text(data['choosan_answer'].toString()),
+                      Text(
+                        data['correct_answer'].toString(),
+                        style: const TextStyle(
+                            color: Color.fromARGB(255, 192, 136, 202),
+                            fontSize: 15),
+                      ),
+                      Text(
+                        data['choosan_answer'].toString(),
+                        style: const TextStyle(
+                            color: Color.fromARGB(146, 0, 0, 0), fontSize: 15),
+                      ),
                     ],
                   ),
                 ),
